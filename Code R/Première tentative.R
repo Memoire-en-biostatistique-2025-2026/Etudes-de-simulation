d2eee2e37a58e1f382049f369f9636fa9ad66e3c
Mouna-Hajar <mounamoha29@gmail.com>
2025-10-01 00:03:46 -0400

    # Première tentaive de génération des données par deux méthodes différentes sur R.

--- Code R/Première tentative.R
+++ Code R/Première tentative.R
@@ -6,45 +6,75 @@
 
 library(simDAG)
 library(ggplot2)
-library(ggforce) # Pour afficher le DAG
+library(ggforce) # Pour afficher le DAG pour la deuxième méthode de génération
 
 datagen <- function(seed = sample(1:1000000, size = 1), ssize = 5000, 
                     
-                    popsize = 15000000, Or_C = 3, OR_W1 = 1, OR_W2 = 2.5,
-                    
-                    em = 0.25, cfV0 = FALSE, cfV1 = FALSE, return_full = FALSE,
-                    
-                    co_inf_para = 0.00001) {
+                    popsize = 15000000) {
   
           
           # Génération du facteur de confusion continu C 
           
           C <- runif(n = popsize, 0.1, 3); # On commence par les noeuds racines de notre DAG
           
-          # Génération du statut vaccinal V : V ~ Bernoulli(logit(10 + 0.3*C))
+          # Génération du statut vaccinal V : V ~ Bernoulli(logit(a0 + a1*C))
           
-          if (cfV0 == TRUE) {
             
-            V <- rep(0, popsize)
-            
-          } else if (cfV1 == TRUE) {
-            
-            V <- rep(1, popsize)
-            
-          } else {
-            
-            V <- rbinom(n = popsize, size = 1, prob = plogis(10 + 0.3*C))
-            
-          }
-   
-}
+            V <- rbinom(n = popsize, size = 1, prob = plogis(-12.25 + 0.3*C))
+          
+          # Génération des infections I1 : I1 ~ Bernoulli(logit(a0 + a1*C)) et 
+          #                           I2 : I2 ~ Bernoulli(logit(a0 + a1*C + a2*V))
+          
 
+          I1 <- rbinom(n = popsize, size = 1, prob = plogis(-11.51292 + 0.35*C))  
+          
+          I2 <- rbinom(n = popsize, size = 1, prob = plogis(-11.51292 + 0.15*C - 0.1*V))  
+          
+          # Génération des symptomes W1: W1~  et W2: W2~
+          
+          W1 <- rep(0, popsize)
+          
+          W1[I1 == 1] <- rbinom(
+            
+            n = sum(I1 == 1),
+            size = 1,
+            prob = plogis(-0.5 + 0.5 * C[I1 == 1])
+            
+          )
+          
+          W2 <- rep(0, popsize)
+          
+          W2[I2 == 1] <- rbinom(
+            
+            n = sum(I2 == 1),
+            size = 1,
+            prob = plogis(-3.75 + 2*C[I2 == 1] - 0.91*V[I2 == 1])
+            
+          )
+          
+          # Génération de W
+          
+          W <- pmax(W1, W2)
+          
+          # Génération de l'hospitalization
+          
+          H = rep(0, popsize)
+          
+          H[W == 1] <- rbinom(prob = plogis(-1.5 + 0.5*C[W == 1]),
+                              size = 1, n = sum(W == 1))
+   
+
+}
+################################################################################
 
 dag <- empty_dag() +
         
        node("C", type = "runif", 0.1, 3) +
        
-      node("V", type = "binomial", formula = ~ 10 + 0.3*C)
+       node("V", type = "binomial", formula = ~ -12.25 + 0.3*C) +
+       
+       node("I1", type = "binomial", formula = ~ -11.51292 + 0.35*C) 
+
   
 set.seed(10)
 
