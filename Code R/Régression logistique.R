a830ad81236b168b3cfcc84d7b194aae13cfc99f
Mouna-Hajar <mounamoha29@gmail.com>
2025-10-16 09:54:06 -0400

    Etapes 3-5.

--- Code R/Régression logistique.R
+++ Code R/Régression logistique.R
@@ -12,3 +12,33 @@
 # 8. Analyser les resultats
 #    - statistiques descriptives
 #    - biais, variance, moyenne de l'erreur-type, couverture des IC
+################################################################################
+################################################################################
+
+# 3. Initialiser des objets pour contenir les resultats
+#    - Coefficient de la regression logistique
+#    - Erreur-type du coefficient
+
+coe_reg <- rep(NA, 1000)
+
+err_reg <- rep(NA, 1000)
+
+# 4. Faire une boucle de 1 a nombre de replications
+# 5. Generation des donnees TND (n = 1000)
+
+co_inf_para <- # Proportion des co-infections
+  
+  set.seed(1) # Pour avoir toujours les mêmes germes
+
+seeds_list <- sample(1:1000000, size = 1000)
+
+l_vraiRRc <- rep(NA, 1000)
+
+for (i in seeds_list) {
+  
+  for (j in 1:1000) {
+    
+    TNDdat <- datagen(seed = i, ssize = 1000, co_inf_para = co_inf_para)
+    
+  }
+}
