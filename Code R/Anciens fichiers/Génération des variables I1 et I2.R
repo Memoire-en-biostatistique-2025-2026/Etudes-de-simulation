45e429d0bf0e7fe1919c010bc36c4c64d8d24c7e
Mouna-Hajar <mounamoha29@gmail.com>
2025-11-20 06:03:08 -0500

    - Calcul de la valeur de b0 pour la génération de la variable I2.

--- Code R/Anciens fichiers/Génération des variables I1 et I2.R
+++ Code R/Anciens fichiers/Génération des variables I1 et I2.R
@@ -7,11 +7,11 @@ library("stats")
 
 a <- 0.5/2.9
 b1 <- 0.3
-b2 <- 0.1
+b2 <- 3
 
-# Définir la fonction en b0 (en calculant l'intégrale)
+# Définir la fonction en b0 (en calculant l'intégrale) pour I1
 
-f <- function (b0) {
+f_I1 <- function (b0) {
   
   (a/b1)*(log(1 + exp(b0 + 3*b1 - b2)) + log(1 + exp(b0 + 3*b1 + b2)) - 
             
@@ -20,6 +20,34 @@ f <- function (b0) {
           0.15 # Contrainte pour la probabilité marginale
   
 }
-uniroot(f, c(-10, 10))
+uniroot(f_I1, c(-10, 10))
 
 help("uniroot")
+
+# Définir la fonction en b0 (en calculant l'intégrale) pour I2
+
+# On pose:
+
+a <- 0.5/2.9
+a0 <- 0.5
+a1 <- 0.3
+b1 <- 0.15
+b2 <- -0.1
+b3 <- -3
+
+f_I2 <- function (b0) {
+  
+  (a/(a1+b1))*(log(1 + exp(a0 + b0 + 3*(a1 + b1) + b2 - b3)) - log(1 + exp(a0 + b0 + 0.1*(a1 + b1) + b2 - b3)) + 
+            
+               log(1 + exp(a0 + b0 + 3*(a1 + b1) - b3)) - log(1 + exp(a0 + b0 + 0.1*(a1 + b1) - b3))+
+              
+               log(1 + exp(a0 + b0 + 3*(a1 + b1) + b2 + b3)) - log(1 + exp(a0 + b0 + 0.1*(a1 + b1) + b2 + b3)) + 
+              
+               log(1 + exp(a0 + b0 + 3*(a1 + b1) + b3)) - log(1 + exp(a0 + b0 + 0.1*(a1 + b1) + b3))) - 
+    
+               0.50 # Contrainte pour la probabilité marginale
+  
+}
+
+uniroot(f_I2, c(-10, 10))
+
