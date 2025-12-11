8342c37163acb370a9fe68964f8f0b2dd307f90a
Mouna-Hajar <mounamoha29@gmail.com>
2025-12-11 08:26:50 -0500

    - Commencer à travailler sur la régression Lasso.
    -Révision du code pour le TNDDR.

--- Code R/Anciens fichiers/TND_Doubly _Robust.R
+++ Code R/Anciens fichiers/TND_Doubly _Robust.R
@@ -19,6 +19,7 @@ library(randomForest)
 library(dplyr)
 library(ranger)
 library(earth)
+library(glmnet)
 
 ################################################################################
 
@@ -54,7 +55,7 @@ RandomForest <- function(dat) {
   mod_g1_ctr <- ranger(
     
     V ~ .,
-    TNDdata = subset(TNDdat_ctr1, select = -Y),
+    data = subset(TNDdat_ctr1, select = -Y),
     num.trees = 200,  # Nombre d'arbres = 200
     mtry = 1,         # Set mtry to 1 for 2-3 covariates
     min.node.size = 60,  # Taille minimale d'un noeud
@@ -73,7 +74,7 @@ RandomForest <- function(dat) {
   mod_g2_ctr <- ranger(
     
     V ~ .,
-    TNDdata = subset(TNDdat_ctr2, select = -Y),
+    data = subset(TNDdat_ctr2, select = -Y),
     num.trees = 200,  
     mtry = 1,         
     min.node.size = 60,  
@@ -107,7 +108,7 @@ RandomForest <- function(dat) {
   # Deuxième étape : Estimer les fonctions  P_TND(Y = 1/ V = v, C = c)  
   ## Entainement du modèle de forêt aléatoire
   
-  ### Sur TNDdat_train1
+  ### Sur le premier ensemble d'entraînement
   
   Out_mu1 <- ranger(
     
@@ -119,7 +120,7 @@ RandomForest <- function(dat) {
     
   )
   
-  ### Sur TNDdat_train2
+  ### Sur le deuxième ensemble d'entraînement
   
   Out_mu2 <- ranger(
     
@@ -170,7 +171,10 @@ RandomForest <- function(dat) {
   
   mu0[s] <- predict(Out_mu2, TNDdata = newTNDdata_mu0_train1)$predictions[, 2]
   
-  # Entraînemet du modèle pour m0 (1 - Y ou P(Y = 0)) 
+  # Deuxième étape : Estimer les fonctions  m0 (1 - Y ou P(Y = 0))  
+  ## Entrainement du modèle de forêt aléatoire
+  
+  ### Sur TNDdat_ctr1   
   
   Out_m1 <- ranger(
     
@@ -182,6 +186,8 @@ RandomForest <- function(dat) {
     
   )
   
+  ### Sur TNDdat_ctr2
+  
   Out_m2 <- ranger(
     
     Y ~ .,
@@ -192,11 +198,10 @@ RandomForest <- function(dat) {
     
   )
   
-  # Initialiser m0 pour contenir les prédictions
+  ## Prédiction des probabilités sur les ensembles tests
+  # Stockage des résultats
   
   m0 <- rep(NA, nrow(TNDdat))
-  
-  # Prédire mu0: P(Y = 0)
   
   m0[-s] <- 1 - predict(Out_m1, TNDdata = select(TNDdat2, -c(V, Y)))$predictions[, 2]
   m0[s] <- 1 - predict(Out_m2, TNDdata = select(TNDdat1, -c(V, Y)))$predictions[, 2]
@@ -211,6 +216,193 @@ RandomForest <- function(dat) {
   
 }
 
+################################################################################
+################################################################################
+
+# Définir une fonction pour prédire les probabilités (Lasso)
+
+RandomForest <- function(dat) {
+  
+  TNDdat <- data.frame(C = dat$C, V = dat$V, Y = dat$Infec_RSV*dat$W2*dat$H)
+ 
+  # Première étape : Diviser aléatoirement le jeu de données en deux parties égales 
+  # Le double cross-fit
+  
+  set.seed(1) # pour que le résultat soit reproductible
+  
+  s <- sample(1:nrow(TNDdat), nrow(TNDdat) / 2)
+  TNDdat_train1 <- TNDdat[s, ]
+  TNDdat_train2 <- TNDdat[-s, ]
+  
+  # Deuxième étape : Estimer les fonctions  P_TND(V = v/ C = c, Y = 0)  
+  ## Entrainement du modèle 
+  
+  ### Sur TNDdat_ctr1 
+  
+  TNDdat_ctr1 <- subset(TNDdat_train1, Y == 0)
+  
+  mod_g1_ctr <- cv.glmnet(
+    
+    V,
+    X = data.matrix(subset(TNDdat_ctr1, select = -Y)),
+    alpha = 1, 
+    lambda = 0.2,
+    standardize = TRUE
+    
+  )
+  
+  ### Sur TNDdat_ctr2
+  
+  # Sous-ensemble : témoins (Y == 0)
+  
+  TNDdat_ctr2 <- subset(TNDdat_train2, Y == 0)
+  
+  mod_g1_ctr <- cv.glmnet(
+    
+    V,
+    X = data.matrix(subset(TNDdat_ctr2, select = -Y)),
+    alpha = 1, 
+    lambda = 0.2,
+    standardize = TRUE
+    
+  )
+  
+  ## Prédiction des probabilités sur les ensembles tests
+  # Stockage des résultats
+  
+  g1_cont <- rep(NA, nrow(TNDdat))  
+  
+  # S'assurer que newTNDdata et TNDdata ont la même structure
+  
+  newTNDdata_train2 <- TNDdat_train2 %>%
+    mutate(V = factor(rep(1, nrow(TNDdat_train2)), levels = levels(TNDdat$V)))
+  
+  # Prédire sur newTNDdata_train2 (ensemble autre que celui utilisé pour le premier entraînement du modèle)
+  
+  g1_cont[-s] <- predict(mod_g1_ctr, TNDdata = newTNDdata_train2)$predictions[, 2]
+  
+  newTNDdata_train1 <- TNDdat1 %>%
+    mutate(V = factor(rep(1, nrow(TNDdat1)), levels = levels(TNDdat$V)))
+  
+  # Prédire sur newTNDdata_train2 (ensemble autre que celui utilisé pour le deuxième entraînement du modèle)
+  
+  g1_cont[s] <- predict(mod_g2_ctr, TNDdata = newTNDdata_train1)$predictions[, 2]
+  
+  # Deuxième étape : Estimer les fonctions  P_TND(Y = 1/ V = v, C = c)  
+  ## Entainement du modèle de forêt aléatoire
+  
+  ### Sur TNDdat_train1
+  
+  Out_mu1 <- cv.glmnet(
+    
+    Y,
+    X = data.matrix(TNDdat_train1),
+    alpha = 1, 
+    lambda = 0.2,
+    standardize = TRUE
+    
+  )
+  
+  ### Sur TNDdat_train2
+  
+  Out_mu1 <- cv.glmnet(
+    
+    Y,
+    X = data.matrix(TNDdat_train1),
+    alpha = 1, 
+    lambda = 0.2,
+    standardize = TRUE
+    
+  )
+  
+  ## Prédire les probabilités sur les ensembles tests 
+  # Stockage des résultats
+  
+  mu1 <- rep(NA, nrow(TNDdat))
+  mu0 <- rep(NA, nrow(TNDdat))
+  
+  # Prédire mu1: P(Y = 1/ V = 1) sur newTNDdata_mu1_train2
+  
+  newTNDdata_mu1_train2 <- TNDdat_train2 %>%
+    select(-Y) %>%
+    mutate(V = factor(rep(1, nrow(TNDdat_train2)), levels = levels(TNDdat$V)))
+  
+  mu1[-s] <- predict(Out_mu1, TNDdata = newTNDdata_mu1_train2)$predictions[, 2]
+  
+  # Prédire mu1: P(Y = 1/ V = 1) sur newTNDdata_mu1_train1
+  
+  newTNDdata_mu1_train1 <- TNDdat_train1 %>%
+    select(-Y) %>%
+    mutate(V = factor(rep(1, nrow(TNDdat_train1)), levels = levels(TNDdat$V)))
+  
+  
+  mu1[s] <- predict(Out_mu2, TNDdata = newTNDdata_mu1_train1)$predictions[, 2]
+  
+  # Prédire mu0: P(Y = 1/ V = 0) sur newTNDdata_mu1_train2
+  
+  newTNDdata_mu0_train2 <- TNDdat2 %>%
+    select(-Y) %>%
+    mutate(V = factor(rep(0, nrow(TNDdat_train2)), levels = levels(TNDdat$V)))
+  
+  mu0[-s] <- predict(Out_mu1, TNDdata = newTNDdata_mu0_train2)$predictions[, 2]
+  
+  # Prédire mu0: P(Y = 1/ V = 0) sur newTNDdata_mu1_train1
+  
+  newTNDdata_mu0_train1 <- TNDdat1 %>%
+    select(-Y) %>%
+    mutate(V = factor(rep(0, nrow(TNDdat1)), levels = levels(TNDdat$V)))
+  
+  mu0[s] <- predict(Out_mu2, TNDdata = newTNDdata_mu0_train1)$predictions[, 2]
+  
+  # Deuxième étape : Estimer les fonctions  m0 (1 - Y ou P(Y = 0))  
+  ## Entrainement du modèle de forêt aléatoire
+  
+  ### Sur TNDdat_ctr1  
+  
+  Out_m1 <- cv.glmnet(
+    
+    Y,
+    X <- data.matrix(subset(TNDdat_ctr1, select = -V)),
+    alpha = 1, 
+    lambda = 0.2,
+    standardize = TRUE
+    
+  )
+  
+  ### Sur TNDdat_ctr2
+  
+  Out_m1 <- cv.glmnet(
+    
+    Y,
+    X <- data.matrix(subset(TNDdat_ctr2, select = -V)),
+    alpha = 1, 
+    lambda = 0.2,
+    standardize = TRUE
+    
+  )
+  
+  # Initialiser m0 pour contenir les prédictions
+  
+  m0 <- rep(NA, nrow(TNDdat))
+  
+  # Prédire mu0: P(Y = 0)
+  
+  m0[-s] <- 1 - predict(Out_m1, TNDdata = select(TNDdat_ctr2, -c(V, Y)))$predictions[, 2]
+  m0[s] <- 1 - predict(Out_m2, TNDdata = select(TNDdat_ctr1, -c(V, Y)))$predictions[, 2]
+  
+  mu1 <- pmin(pmax(mu1, 0.0000001), 0.9999999)
+  mu0 <- pmin(pmax(mu0, 0.0000001), 0.9999999)
+  m0 <- pmin(pmax(m0, 0.0000001), 0.9999999)
+  g1 <- pmin(pmax(g1_cont, 0.0000001), 0.9999999)
+  g0 <- 1 - pmin(pmax(g1_cont, 0.0000001), 0.9999999)
+  
+  return(list(mu1 = mu1, mu0 = mu0, m0 = m0, g1 = g1, g0 = g0, w1 = m0 / (1 - mu1), w0 = m0 / (1 - mu0)))
+  
+}
+
+
+
+################################################################################
 ################################################################################
 
 # Définir une fonction pour prédire les probabilités (earth_GLM)
