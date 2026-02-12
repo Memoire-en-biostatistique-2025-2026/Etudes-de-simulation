120604bdcd3a8494628f9b1a36423cef29c27263
Mouna-Hajar <mounamoha29@gmail.com>
2026-02-12 09:29:43 -0500

    - Tableaux de comparaison des résultats.

--- Code R/Anciens fichiers/Rapport des résultats.R
+++ Code R/Anciens fichiers/Rapport des résultats.R
@@ -12,55 +12,150 @@ load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'hiver 2026/A
 
 # Tableau de comparaison 
 
-comparaison <- data.frame(matrix(ncol = 5, 
-                                 nrow = 30))
+comparaison01 <- data.frame(matrix(ncol = 5, 
+                                   nrow = 30))
 
-colnames(comparaison) <- c(
-                         "Estimation", 
-                         "Performance",
-                         "Scénario01",
-                         "Scénario02",
-                         "Scénario03"
+colnames(comparaison01) <- c(
+  "Estimation", 
+  "Performance",
+  "Scénario01",
+  "Scénario02",
+  "Scénario03"
 )
 
-comparaison$Estimation <- c("RegLog", "-", "-", "-", "-", "IPW", "-", "-", "-", "-", 
-                            "TNDDR_RF", "-", "-", "-", "-", "TNDDR_Lasso", "-", "-", "-", "-",
-                            "TNDDR_Mars", "-", "-", "-", "-", "TNDDR_RN", "-", "-", "-", "-")
+comparaison01$Estimation <- c("RegLog", "-", "-", "-", "-", "IPW", "-", "-", "-", "-", 
+                              "TNDDR_RF", "-", "-", "-", "-", "TNDDR_Lasso", "-", "-", "-", "-",
+                              "TNDDR_Mars", "-", "-", "-", "-", "TNDDR_RN", "-", "-", "-", "-")
 
-comparaison$Performance <- rep(c("Biais", "Variance", "MSE", "Précision", "%Cov"), 6)
+comparaison01$Performance <- rep(c("Biais", "Variance", "MSE", "Précision", "%Cov"), 6)
 
-a <- as.data.frame(c(Tab1$Autres, Tab1$`Erreur de Monte Carlo`))
-b <- as.data.frame(c(Tab2$Autres, Tab2$`Erreur de Monte Carlo`))
-c <- as.data.frame(c(Tab3$Autres, Tab3$`Erreur de Monte Carlo`))
-
-comparaison$Scénario01 <- c(a$bias, a$var, a$mse, a$value, a$coverage,
-                            b$bias, b$var, b$mse, b$value, b$coverage,
-                            c$bias, c$var, c$mse, c$value, c$coverage,
-                            c$bias.1, c$var.1, c$mse.1, c$value.1, c$coverage.1,
-                            c$bias.2, c$var.2, c$mse.2, c$value.2, c$coverage.2,
-                            c$bias.3, c$var.3, c$mse.3, c$value.3, c$coverage.3)
+comparaison01$Scénario01 <- c(
   
-a <- as.data.frame(c(Tab1_2$Autres, Tab1_2$`Erreur de Monte Carlo`))
-b <- as.data.frame(c(Tab2_2$Autres, Tab2_2$`Erreur de Monte Carlo`))
-c <- as.data.frame(c(Tab3_2$Autres, Tab3_2$`Erreur de Monte Carlo`))
+  Tab1$Autres, Tab1$`Erreur de Monte Carlo`[4],
+  Tab2$Autres, Tab2$`Erreur de Monte Carlo`[4],
+  Tab3$Autres[1:4], Tab3$`Erreur de Monte Carlo`[4],
+  Tab3$Autres[5:8], Tab3$`Erreur de Monte Carlo`[8],
+  Tab3$Autres[9:12], Tab3$`Erreur de Monte Carlo`[12],
+  Tab3$Autres[13:16], Tab3$`Erreur de Monte Carlo`[16]
+  
+)
 
-comparaison$Scénario02 <- c(a$value.bias[1], a$value.var[1], a$value.mse[1], a$value[1], a$value.coverage[1],
-                            b$value.bias[1], b$value.var[1], b$value.mse[1], b$value[1], b$value.coverage[1],
-                            c$value.bias[1], c$value.var[1], c$value.mse[1], c$value[1], c$value.coverage[1],
-                            c$value.bias.3[1], c$value.var.3[1], c$value.mse.3[1], c$value.1[1], c$value.coverage.3[1],
-                            c$value.bias.6[1], c$value.var.6[1], c$value.mse.6[1], c$value.2[1], c$value.coverage.6[1],
-                            c$value.bias.9[1], c$value.var.9[1], c$value.mse.9[1], c$value.3[1], c$value.coverage.9[1])
+comparaison01$Scénario02 <- c(
+  
+  Tab1_2$Autres, Tab1_2$`Erreur de Monte Carlo`[4],
+  Tab2_2$Autres, Tab2_2$`Erreur de Monte Carlo`[4],
+  Tab3_2$Autres[1:4], Tab3_2$`Erreur de Monte Carlo`[4],
+  Tab3_2$Autres[5:8], Tab3_2$`Erreur de Monte Carlo`[8],
+  Tab3_2$Autres[9:12], Tab3_2$`Erreur de Monte Carlo`[12],
+  Tab3_2$Autres[13:16], Tab3_2$`Erreur de Monte Carlo`[16]
+  
+)
 
-a <- as.data.frame(c(Tab1_3$Autres, Tab1_3$`Erreur de Monte Carlo`))
-b <- as.data.frame(c(Tab2_3$Autres, Tab2_3$`Erreur de Monte Carlo`))
-c <- as.data.frame(c(Tab3_3$Autres, Tab3_3$`Erreur de Monte Carlo`))
+comparaison01$Scénario03 <- c(
+  
+  Tab1_3$Autres, Tab1_3$`Erreur de Monte Carlo`[4],
+  Tab2_3$Autres, Tab2_3$`Erreur de Monte Carlo`[4],
+  Tab3_3$Autres[1:4], Tab3_3$`Erreur de Monte Carlo`[4],
+  Tab3_3$Autres[5:8], Tab3_3$`Erreur de Monte Carlo`[8],
+  Tab3_3$Autres[9:12], Tab3_3$`Erreur de Monte Carlo`[12],
+  Tab3_3$Autres[13:16], Tab3_3$`Erreur de Monte Carlo`[16]
+  
+)
 
-comparaison$Scénario03 <- c(a$value.bias[1], a$value.var[1], a$value.mse[1], a$value[1], a$value.coverage[1],
-                            b$value.bias[1], b$value.var[1], b$value.mse[1], b$value[1], b$value.coverage[1],
-                            c$value.bias[1], c$value.var[1], c$value.mse[1], c$value[1], c$value.coverage[1],
-                            c$value.bias.3[1], c$value.var.3[1], c$value.mse.3[1], c$value.1[1], c$value.coverage.3[1],
-                            c$value.bias.6[1], c$value.var.6[1], c$value.mse.6[1], c$value.2[1], c$value.coverage.6[1],
-                            c$value.bias.9[1], c$value.var.9[1], c$value.mse.9[1], c$value.3[1], c$value.coverage.9[1])
-################################################################################
+# nsim = 1000, couverture vaccinale  ~ 33% : Scénarios 01, 02 et 03
 
-View(comparaison)
+comparaison01$Scénario01 <- sapply(comparaison01$Scénario01, FUN = function(x)x[[2]])
+comparaison01$Scénario02 <- sapply(comparaison01$Scénario02, FUN = function(x)x[[2]][[1]])
+comparaison01$Scénario03 <- sapply(comparaison01$Scénario03, FUN = function(x)x[[2]][[1]])
+
+View(comparaison01)
+
+# nsim = 1000, coouverture vaccinale  ~ 33%, 50% et 70% : Scénarios 02 et 03
+
+comparaison02 <- data.frame(matrix(ncol = 8, 
+                                   nrow = 30))
+
+colnames(comparaison02) <- c(
+  "Estimation", 
+  "Performance",
+  "Scénario02_33%",
+  "Scénario02_50%",
+  "Scénario02_70%",
+  "Scénario03_33%",
+  "Scénario03_50%",
+  "Scénario03_70%"
+)
+
+comparaison02$Estimation <- c("RegLog", "-", "-", "-", "-", "IPW", "-", "-", "-", "-", 
+                              "TNDDR_RF", "-", "-", "-", "-", "TNDDR_Lasso", "-", "-", "-", "-",
+                              "TNDDR_Mars", "-", "-", "-", "-", "TNDDR_RN", "-", "-", "-", "-")
+
+comparaison02$Performance <- rep(c("Biais", "Variance", "MSE", "Précision", "%Cov"), 6)
+
+comparaison02$`Scénario02_33%` <- c(
+  
+  sapply(Tab1_2$Autres, function(x) x[[2]][[1]]), Tab1_2$`Erreur de Monte Carlo`[[4]][[2]][[1]],
+  sapply(Tab1_2$Autres, function(x) x[[2]][[1]]), Tab2_2$`Erreur de Monte Carlo`[[4]][[2]][[1]],
+  sapply(Tab3_2$Autres[1:4],function(x) x[[2]][[1]]) , Tab3_2$`Erreur de Monte Carlo`[[4]][[2]][[1]],
+  sapply(Tab3_2$Autres[5:8],function(x) x[[2]][[1]]), Tab3_2$`Erreur de Monte Carlo`[[8]][[2]][[1]],
+  sapply(Tab3_2$Autres[9:12],function(x) x[[2]][[1]]), Tab3_2$`Erreur de Monte Carlo`[[12]][[2]][[1]],
+  sapply(Tab3_2$Autres[13:16],function(x) x[[2]][[1]]), Tab3_2$`Erreur de Monte Carlo`[[16]][[2]][[1]]
+  
+)
+
+comparaison02$`Scénario02_50%` <- c(
+  
+  sapply(Tab1_2$Autres, function(x) x[[2]][[2]]), Tab1_2$`Erreur de Monte Carlo`[[4]][[2]][[2]],
+  sapply(Tab1_2$Autres, function(x) x[[2]][[2]]), Tab2_2$`Erreur de Monte Carlo`[[4]][[2]][[2]],
+  sapply(Tab3_2$Autres[1:4],function(x) x[[2]][[2]]) , Tab3_2$`Erreur de Monte Carlo`[[4]][[2]][[2]],
+  sapply(Tab3_2$Autres[5:8],function(x) x[[2]][[2]]), Tab3_2$`Erreur de Monte Carlo`[[8]][[2]][[2]],
+  sapply(Tab3_2$Autres[9:12],function(x) x[[2]][[2]]), Tab3_2$`Erreur de Monte Carlo`[[12]][[2]][[2]],
+  sapply(Tab3_2$Autres[13:16],function(x) x[[2]][[2]]), Tab3_2$`Erreur de Monte Carlo`[[16]][[2]][[2]]
+  
+)
+
+comparaison02$`Scénario02_70%` <- c(
+  
+  sapply(Tab1_2$Autres, function(x) x[[2]][[3]]), Tab1_2$`Erreur de Monte Carlo`[[4]][[2]][[3]],
+  sapply(Tab1_2$Autres, function(x) x[[2]][[3]]), Tab2_2$`Erreur de Monte Carlo`[[4]][[2]][[3]],
+  sapply(Tab3_2$Autres[1:4],function(x) x[[2]][[3]]) , Tab3_2$`Erreur de Monte Carlo`[[4]][[2]][[3]],
+  sapply(Tab3_2$Autres[5:8],function(x) x[[2]][[3]]), Tab3_2$`Erreur de Monte Carlo`[[8]][[2]][[3]],
+  sapply(Tab3_2$Autres[9:12],function(x) x[[2]][[3]]), Tab3_2$`Erreur de Monte Carlo`[[12]][[2]][[3]],
+  sapply(Tab3_2$Autres[13:16],function(x) x[[2]][[3]]), Tab3_2$`Erreur de Monte Carlo`[[16]][[2]][[3]]
+  
+)
+
+comparaison02$`Scénario03_33%` <- c(
+  
+  sapply(Tab1_3$Autres, function(x) x[[2]][[1]]), Tab1_3$`Erreur de Monte Carlo`[[4]][[2]][[1]],
+  sapply(Tab1_3$Autres, function(x) x[[2]][[1]]), Tab2_3$`Erreur de Monte Carlo`[[4]][[2]][[1]],
+  sapply(Tab3_3$Autres[1:4],function(x) x[[2]][[1]]) , Tab3_3$`Erreur de Monte Carlo`[[4]][[2]][[1]],
+  sapply(Tab3_3$Autres[5:8],function(x) x[[2]][[1]]), Tab3_3$`Erreur de Monte Carlo`[[8]][[2]][[1]],
+  sapply(Tab3_3$Autres[9:12],function(x) x[[2]][[1]]), Tab3_3$`Erreur de Monte Carlo`[[12]][[2]][[1]],
+  sapply(Tab3_3$Autres[13:16],function(x) x[[2]][[1]]), Tab3_3$`Erreur de Monte Carlo`[[16]][[2]][[1]]
+  
+)
+
+comparaison02$`Scénario03_50%` <- c(
+  
+  sapply(Tab1_3$Autres, function(x) x[[2]][[2]]), Tab1_3$`Erreur de Monte Carlo`[[4]][[2]][[2]],
+  sapply(Tab1_3$Autres, function(x) x[[2]][[2]]), Tab2_3$`Erreur de Monte Carlo`[[4]][[2]][[2]],
+  sapply(Tab3_3$Autres[1:4],function(x) x[[2]][[2]]) , Tab3_3$`Erreur de Monte Carlo`[[4]][[2]][[2]],
+  sapply(Tab3_3$Autres[5:8],function(x) x[[2]][[2]]), Tab3_3$`Erreur de Monte Carlo`[[8]][[2]][[2]],
+  sapply(Tab3_3$Autres[9:12],function(x) x[[2]][[2]]), Tab3_3$`Erreur de Monte Carlo`[[12]][[2]][[2]],
+  sapply(Tab3_3$Autres[13:16],function(x) x[[2]][[2]]), Tab3_3$`Erreur de Monte Carlo`[[16]][[2]][[2]]
+  
+)
+
+comparaison02$`Scénario03_70%` <- c(
+  
+  sapply(Tab1_3$Autres, function(x) x[[2]][[3]]), Tab1_3$`Erreur de Monte Carlo`[[4]][[2]][[3]],
+  sapply(Tab1_3$Autres, function(x) x[[2]][[3]]), Tab2_3$`Erreur de Monte Carlo`[[4]][[2]][[3]],
+  sapply(Tab3_3$Autres[1:4],function(x) x[[2]][[3]]) , Tab3_3$`Erreur de Monte Carlo`[[4]][[2]][[3]],
+  sapply(Tab3_3$Autres[5:8],function(x) x[[2]][[3]]), Tab3_3$`Erreur de Monte Carlo`[[8]][[2]][[3]],
+  sapply(Tab3_3$Autres[9:12],function(x) x[[2]][[3]]), Tab3_3$`Erreur de Monte Carlo`[[12]][[2]][[3]],
+  sapply(Tab3_3$Autres[13:16],function(x) x[[2]][[3]]), Tab3_3$`Erreur de Monte Carlo`[[16]][[2]][[3]]
+  
+)
+
+View(comparaison02)
