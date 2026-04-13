ef9f532da5aa7f486f71fce1174277e957fad5ad
Mouna-Hajar <mounamoha29@gmail.com>
2026-04-12 22:10:26 -0400

    -Mise à jour des résultats.

--- Code R/Fonc03_TNDDR.R
+++ Code R/Fonc03_TNDDR.R
@@ -383,11 +383,6 @@ Lasso <- function(dat) {
   
   TNDdat <- data.frame(C = dat$C, V = dat$V, Y = dat$Infec_RSV)
   
-  # Convert the variables V and Y (response variables) into factors for classification
-  
-  TNDdat$V <- as.factor(TNDdat$V)
-  TNDdat$Y <- as.factor(TNDdat$Y)
-  
   # Step 1: Randomly split the dataset into two equal parts 
   # Double CrossFit
   
