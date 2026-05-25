48d98b494bc345316e877974443a05b8591e01ec
Mouna-Hajar <mounamoha29@gmail.com>
2026-05-25 07:36:14 -0400

    - Régression du biais pour la méthode de régression logistique (RRc).

---
+++ Code R/Anciens fichiers/Régression_Biais-moyen.R
@@ -0,0 +1,119 @@
+# Chargement des librairies nécessaires
+
+# Chargement des résultats bruts
+
+## Taille d'échantillo = 1000 ##
+
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario01.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario02.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario03.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario04.RData")
+
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario05_10%.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario05_15%.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario05_30%.RData")
+
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario06_50%.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario06_70%.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario06_85%.RData")
+
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario07_10%.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario07_30%.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario07_50%.RData")
+
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario08_5%.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario08_10%.RData")
+
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario09_10%.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario09_15%.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario09_20%.RData")
+
+## Taille d'échantillo = 5000 ##
+
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario01_5000.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario02_5000.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario03_5000.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario04_5000.RData")
+
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario05_10%_5000.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario05_15%_5000.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario05_30%_5000.RData")
+
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario06_50%_5000.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario06_70%_5000.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario06_85%_5000.RData")
+
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario07_10%_5000.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario07_30%_5000.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario07_50%_5000.RData")
+
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario08_5%_5000.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario08_10%_5000.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario08_20%_5000.RData")
+
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario09_10%_5000.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario09_15%_5000.RData")
+load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario09_20%_5000.RData")
+
+# Création du jeu de données
+
+df <- data.frame(matrix(ncol = 9, 
+                                   nrow = 38))
+
+colnames(df) <- c(
+  "Biais", 
+  "Taille",
+  
+  "TCI_TND",
+  "TCI_POP",
+  
+  "I2",
+  "I1",
+  
+  "CV",
+  
+  "W2",
+  "W1"
+)
+
+df$Biais <- c(Tab01_1$Autres[[1]]$value, Tab01_2$Autres[[1]]$value, Tab01_3$Autres[[1]]$value, Tab01_4$Autres[[1]]$value,
+              Tab01_5_1$Autres[[1]]$value, Tab01_5_2$Autres[[1]]$value, Tab01_5_3$Autres[[1]]$value,
+              Tab01_6_1$Autres[[1]]$value, Tab01_6_2$Autres[[1]]$value, Tab01_6_3$Autres[[1]]$value,
+              Tab01_7_1$Autres[[1]]$value, Tab01_7_2$Autres[[1]]$value, Tab01_7_3$Autres[[1]]$value,
+              Tab01_8_1$Autres[[1]]$value, Tab01_8_2$Autres[[1]]$value, NA,
+              Tab01_9_1$Autres[[1]]$value, Tab01_9_2$Autres[[1]]$value, Tab01_9_3$Autres[[1]]$value,
+              
+              Tab01_1_5000$Autres[[1]]$value, Tab01_2_5000$Autres[[1]]$value, Tab01_3_5000$Autres[[1]]$value, Tab01_4_5000$Autres[[1]]$value,
+              Tab01_5_1_5000$Autres[[1]]$value, Tab01_5_2_5000$Autres[[1]]$value, Tab01_5_3_5000$Autres[[1]]$value,
+              Tab01_6_1_5000$Autres[[1]]$value, Tab01_6_2_5000$Autres[[1]]$value, Tab01_6_3_5000$Autres[[1]]$value,
+              Tab01_7_1_5000$Autres[[1]]$value, Tab01_7_2_5000$Autres[[1]]$value, Tab01_7_3_5000$Autres[[1]]$value,
+              Tab01_8_1_5000$Autres[[1]]$value, Tab01_8_2_5000$Autres[[1]]$value, Tab01_8_3_5000$Autres[[1]]$value,
+              Tab01_9_1_5000$Autres[[1]]$value, Tab01_9_2_5000$Autres[[1]]$value, Tab01_9_3_5000$Autres[[1]]$value)
+
+df$Taille <- c(rep(1000, 19), rep(5000, 19))
+
+df$TCI_TND <- c(0, 0.1, 0.2, 0.4, rep(0.3, 15), 0, 0.1, 0.2, 0.4, rep(0.3, 15))
+
+
+df$TCI_POP <- c(0, 0.02, 0.05, 0.11, 0.03, 0.04, 0.05, 0.07, 0.1, 0.14, rep(0.07, 3), 0.08, 0.07, 0.05
+                , 0.07, 0.06, 0.06, 0, 0.02, 0.05, 0.11, 0.03, 0.04, 0.05, 0.07, 0.1, 0.14, rep(0.07, 3), 0.08, 0.07, 0.05
+                , 0.07, 0.06, 0.06)
+
+df$I2 <- c(rep(0.5, 4), 0.1, 0.15, 0.3, rep(0.5, 12), rep(0.5, 4), 0.1, 0.15, 0.3, rep(0.5, 12))
+
+df$I1 <- c(rep(0.15, 7), 0.1, 0.3, 0.5, rep(0.15, 9), rep(0.15, 7), 0.1, 0.3, 0.5, rep(0.15, 9))
+
+df$CV <- c(rep(0.35, 10), 0.5, 0.7, 0.85, rep(0.35, 6), rep(0.35, 10), 0.5, 0.7, 0.85, rep(0.35, 6))
+
+df$W2 <- c(rep(0.02, 13), 0.05, 0.1, 0.2, rep(0.5, 3), rep(0.02, 13), 0.05, 0.1, 0.2, rep(0.5, 3))
+
+df$W1 <- c(rep(0.05, 16), 0.1, 0.15, 0.2, rep(0.05, 16), 0.1, 0.15, 0.2)
+
+df$Biais <- sapply(df$Biais, FUN = function(x)x[[1]])
+
+View(df)
+
+# Définition du modèle de régression
+
+modele <- lm(Biais ~ Taille + TCI_TND + TCI_POP + I2 + I1 + CV + W1 +W2, data = df)
+summary(modele)
