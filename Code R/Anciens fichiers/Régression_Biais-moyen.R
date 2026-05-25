553b0c1f81281a696d7a4300d862453344428cdb
Mouna-Hajar <mounamoha29@gmail.com>
2026-05-25 07:45:17 -0400

    - Régressio du biais pour les deux méthodes IPW et MARS.

--- Code R/Anciens fichiers/Régression_Biais-moyen.R
+++ Code R/Anciens fichiers/Régression_Biais-moyen.R
@@ -55,6 +55,10 @@ load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/A
 load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario09_15%_5000.RData")
 load("C:/Users/lenovo/Desktop/Université Laval 2025-2026/Session d'été 2026/Activité de recherche_04/Etudes-de-simulation/Résultats bruts/Résultats_Scénario09_20%_5000.RData")
 
+################################################################################
+########################### Régression logistique ##############################
+################################################################################
+
 # Création du jeu de données
 
 df <- data.frame(matrix(ncol = 9, 
@@ -89,6 +93,140 @@ df$Biais <- c(Tab01_1$Autres[[1]]$value, Tab01_2$Autres[[1]]$value, Tab01_3$Autr
               Tab01_7_1_5000$Autres[[1]]$value, Tab01_7_2_5000$Autres[[1]]$value, Tab01_7_3_5000$Autres[[1]]$value,
               Tab01_8_1_5000$Autres[[1]]$value, Tab01_8_2_5000$Autres[[1]]$value, Tab01_8_3_5000$Autres[[1]]$value,
               Tab01_9_1_5000$Autres[[1]]$value, Tab01_9_2_5000$Autres[[1]]$value, Tab01_9_3_5000$Autres[[1]]$value)
+
+df$Taille <- c(rep(1000, 19), rep(5000, 19))
+
+df$TCI_TND <- c(0, 0.1, 0.2, 0.4, rep(0.3, 15), 0, 0.1, 0.2, 0.4, rep(0.3, 15))
+
+
+df$TCI_POP <- c(0, 0.02, 0.05, 0.11, 0.03, 0.04, 0.05, 0.07, 0.1, 0.14, rep(0.07, 3), 0.08, 0.07, 0.05
+                , 0.07, 0.06, 0.06, 0, 0.02, 0.05, 0.11, 0.03, 0.04, 0.05, 0.07, 0.1, 0.14, rep(0.07, 3), 0.08, 0.07, 0.05
+                , 0.07, 0.06, 0.06)
+
+df$I2 <- c(rep(0.5, 4), 0.1, 0.15, 0.3, rep(0.5, 12), rep(0.5, 4), 0.1, 0.15, 0.3, rep(0.5, 12))
+
+df$I1 <- c(rep(0.15, 7), 0.1, 0.3, 0.5, rep(0.15, 9), rep(0.15, 7), 0.1, 0.3, 0.5, rep(0.15, 9))
+
+df$CV <- c(rep(0.35, 10), 0.5, 0.7, 0.85, rep(0.35, 6), rep(0.35, 10), 0.5, 0.7, 0.85, rep(0.35, 6))
+
+df$W2 <- c(rep(0.02, 13), 0.05, 0.1, 0.2, rep(0.5, 3), rep(0.02, 13), 0.05, 0.1, 0.2, rep(0.5, 3))
+
+df$W1 <- c(rep(0.05, 16), 0.1, 0.15, 0.2, rep(0.05, 16), 0.1, 0.15, 0.2)
+
+df$Biais <- sapply(df$Biais, FUN = function(x)x[[1]])
+
+View(df)
+
+# Définition du modèle de régression
+
+modele <- lm(Biais ~ Taille + TCI_TND + TCI_POP + I2 + I1 + CV + W1 +W2, data = df)
+summary(modele)
+
+################################################################################
+########################### IPW ##############################
+################################################################################
+
+# Création du jeu de données
+
+df <- data.frame(matrix(ncol = 9, 
+                        nrow = 38))
+
+colnames(df) <- c(
+  "Biais", 
+  "Taille",
+  
+  "TCI_TND",
+  "TCI_POP",
+  
+  "I2",
+  "I1",
+  
+  "CV",
+  
+  "W2",
+  "W1"
+)
+
+df$Biais <- c(Tab02_1$Autres[[1]]$value, Tab02_2$Autres[[1]]$value, Tab02_3$Autres[[1]]$value, Tab02_4$Autres[[1]]$value,
+              Tab02_5_1$Autres[[1]]$value, Tab02_5_2$Autres[[1]]$value, Tab02_5_3$Autres[[1]]$value,
+              Tab02_6_1$Autres[[1]]$value, Tab02_6_2$Autres[[1]]$value, Tab02_6_3$Autres[[1]]$value,
+              Tab02_7_1$Autres[[1]]$value, Tab02_7_2$Autres[[1]]$value, Tab02_7_3$Autres[[1]]$value,
+              Tab02_8_1$Autres[[1]]$value, Tab02_8_2$Autres[[1]]$value, NA,
+              Tab02_9_1$Autres[[1]]$value, Tab02_9_2$Autres[[1]]$value, Tab02_9_3$Autres[[1]]$value,
+              
+              Tab02_1_5000$Autres[[1]]$value, Tab02_2_5000$Autres[[1]]$value, Tab02_3_5000$Autres[[1]]$value, Tab02_4_5000$Autres[[1]]$value,
+              Tab02_5_1_5000$Autres[[1]]$value, Tab02_5_2_5000$Autres[[1]]$value, Tab02_5_3_5000$Autres[[1]]$value,
+              Tab02_6_1_5000$Autres[[1]]$value, Tab02_6_2_5000$Autres[[1]]$value, Tab02_6_3_5000$Autres[[1]]$value,
+              Tab02_7_1_5000$Autres[[1]]$value, Tab02_7_2_5000$Autres[[1]]$value, Tab02_7_3_5000$Autres[[1]]$value,
+              Tab02_8_1_5000$Autres[[1]]$value, Tab02_8_2_5000$Autres[[1]]$value, Tab02_8_3_5000$Autres[[1]]$value,
+              Tab02_9_1_5000$Autres[[1]]$value, Tab02_9_2_5000$Autres[[1]]$value, Tab02_9_3_5000$Autres[[1]]$value)
+
+df$Taille <- c(rep(1000, 19), rep(5000, 19))
+
+df$TCI_TND <- c(0, 0.1, 0.2, 0.4, rep(0.3, 15), 0, 0.1, 0.2, 0.4, rep(0.3, 15))
+
+
+df$TCI_POP <- c(0, 0.02, 0.05, 0.11, 0.03, 0.04, 0.05, 0.07, 0.1, 0.14, rep(0.07, 3), 0.08, 0.07, 0.05
+                , 0.07, 0.06, 0.06, 0, 0.02, 0.05, 0.11, 0.03, 0.04, 0.05, 0.07, 0.1, 0.14, rep(0.07, 3), 0.08, 0.07, 0.05
+                , 0.07, 0.06, 0.06)
+
+df$I2 <- c(rep(0.5, 4), 0.1, 0.15, 0.3, rep(0.5, 12), rep(0.5, 4), 0.1, 0.15, 0.3, rep(0.5, 12))
+
+df$I1 <- c(rep(0.15, 7), 0.1, 0.3, 0.5, rep(0.15, 9), rep(0.15, 7), 0.1, 0.3, 0.5, rep(0.15, 9))
+
+df$CV <- c(rep(0.35, 10), 0.5, 0.7, 0.85, rep(0.35, 6), rep(0.35, 10), 0.5, 0.7, 0.85, rep(0.35, 6))
+
+df$W2 <- c(rep(0.02, 13), 0.05, 0.1, 0.2, rep(0.5, 3), rep(0.02, 13), 0.05, 0.1, 0.2, rep(0.5, 3))
+
+df$W1 <- c(rep(0.05, 16), 0.1, 0.15, 0.2, rep(0.05, 16), 0.1, 0.15, 0.2)
+
+df$Biais <- sapply(df$Biais, FUN = function(x)x[[1]])
+
+View(df)
+
+# Définition du modèle de régression
+
+modele <- lm(Biais ~ Taille + TCI_TND + TCI_POP + I2 + I1 + CV + W1 +W2, data = df)
+summary(modele)
+
+################################################################################
+################################ MARS ##########################################
+################################################################################
+
+# Création du jeu de données
+
+df <- data.frame(matrix(ncol = 9, 
+                        nrow = 38))
+
+colnames(df) <- c(
+  "Biais", 
+  "Taille",
+  
+  "TCI_TND",
+  "TCI_POP",
+  
+  "I2",
+  "I1",
+  
+  "CV",
+  
+  "W2",
+  "W1"
+)
+
+df$Biais <- c(Tab03_1$Autres[9:12][[1]]$value, Tab03_2$Autres[9:12][[1]]$value, Tab03_3$Autres[9:12][[1]]$value, Tab03_4$Autres[9:12][[1]]$value,
+              Tab03_5_1$Autres[9:12][[1]]$value, Tab03_5_2$Autres[9:12][[1]]$value, Tab03_5_3$Autres[9:12][[1]]$value,
+              Tab03_6_1$Autres[9:12][[1]]$value, Tab03_6_2$Autres[9:12][[1]]$value, Tab03_6_3$Autres[9:12][[1]]$value,
+              Tab03_7_1$Autres[9:12][[1]]$value, Tab03_7_2$Autres[9:12][[1]]$value, Tab03_7_3$Autres[9:12][[1]]$value,
+              Tab03_8_1$Autres[9:12][[1]]$value, Tab03_8_2$Autres[9:12][[1]]$value, NA,
+              Tab03_9_1$Autres[9:12][[1]]$value, Tab03_9_2$Autres[9:12][[1]]$value, Tab03_9_3$Autres[9:12][[1]]$value,
+              
+              Tab03_1_5000$Autres[9:12][[1]]$value, Tab03_2_5000$Autres[9:12][[1]]$value, Tab03_3_5000$Autres[9:12][[1]]$value, Tab03_4_5000$Autres[9:12][[1]]$value,
+              Tab03_5_1_5000$Autres[9:12][[1]]$value, Tab03_5_2_5000$Autres[9:12][[1]]$value, Tab03_5_3_5000$Autres[9:12][[1]]$value,
+              Tab03_6_1_5000$Autres[9:12][[1]]$value, Tab03_6_2_5000$Autres[9:12][[1]]$value, Tab03_6_3_5000$Autres[9:12][[1]]$value,
+              Tab03_7_1_5000$Autres[9:12][[1]]$value, Tab03_7_2_5000$Autres[9:12][[1]]$value, Tab03_7_3_5000$Autres[9:12][[1]]$value,
+              Tab03_8_1_5000$Autres[9:12][[1]]$value, Tab03_8_2_5000$Autres[9:12][[1]]$value, Tab03_8_3_5000$Autres[9:12][[1]]$value,
+              Tab03_9_1_5000$Autres[9:12][[1]]$value, Tab03_9_2_5000$Autres[9:12][[1]]$value, Tab03_9_3_5000$Autres[9:12][[1]]$value)
 
 df$Taille <- c(rep(1000, 19), rep(5000, 19))
 
